dd9970e11ee745e14d554c697085da0cb940ece3
claudiu <busteagaclaudiu@gmail.com>
2023-03-19 13:28:51 +0000

    updates

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import vue2 from '@vitejs/plugin-vue2'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base:"/lesson_app_cw3/",
+  base:"/cw3-mobileApp/",
 
   plugins: [
     vue2(),
